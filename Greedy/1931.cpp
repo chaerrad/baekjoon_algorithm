49cd18a9c09af82ded6edc58b4e230556e7662f4
byuengyun Chae <chaeby7@gmail.com>
2022-01-13 15:30:15 +0900

    Update baekjoon 1931(Greedy)

---
+++ Greedy/1931.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct Meeting {
+	int begin, end;
+};
+bool cmp(struct Meeting& u, struct Meeting& v) //정렬기준
+{
+	if (u.end == v.end) {
+		return u.begin < v.begin;
+	}
+	else {
+		return u.end < v.end;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	vector<Meeting> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i].begin >> a[i].end;
+	}
+	sort(a.begin(), a.end(), cmp);
+	int now = 0;
+	int ans = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (now <= a[i].begin) {
+			now = a[i].end;
+			ans += 1;
+		}
+	}
+	cout << ans;
+	return 0;
+}
